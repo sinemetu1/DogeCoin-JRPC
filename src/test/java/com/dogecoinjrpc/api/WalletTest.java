b30b57c1a58eceecb644443274e3523d519f3ccf
Sam Garrett <sam@actionx.com>
2013-12-29 17:50:59 -0500

    test - adding tests

--- src/test/java/com/dogecoinjrpc/api/WalletTest.java
+++ src/test/java/com/dogecoinjrpc/api/WalletTest.java
@@ -3,6 +3,7 @@ package com.dogecoinjrpc.api;
 import static org.junit.Assert.*;
 
 import org.apache.log4j.Logger;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Test;
 
@@ -10,30 +11,109 @@ import com.dogecoinjrpc.api.Wallet;
 
 public class WalletTest {
 	private static Logger log = Logger.getLogger(WalletTest.class);
+
+    private final static int version = 60400;
+    private final static int protocolVersion = 60003;
+    private final static int walletVersion = 60000;
+
 	
 	@Test
 	public void testGetInfo() throws Exception {
 		Wallet w = new Wallet();
 		String actual = w.getInfo();
-		JSONObject expected = new JSONObject(
-				"{\"result\":{\"version\":60400,\"protocolversion\":60003,\""
-						+ "walletversion\":60000,\"balance\":0.00000000,\"blocks\":33284,\"connections\":12,"
-						+ "\"proxy\":\"\",\"difficulty\":361.33878958,\"testnet\":false,\"keypoololdest\":1388084960"
-						+ ",\"keypoolsize\":104,\"paytxfee\":0.00000000,\"mininput\":0.01000000,\"errors\":\"\"},"
-						+ "\"error\":null,\"id\":null}");
-		assertTrue(actual.toString().contains("\"error\":null"));
-		assertTrue(actual.toString().contains("\"errors\":\"\""));
-//		log.info("WWW actual:" + actual);
-//		assertEquals(expected, new JSONObject(actual));
+		JSONObject actualJSONObject = new JSONObject(actual);
+		assertEquals(JSONObject.NULL, actualJSONObject.get("error"));
+		JSONObject result = actualJSONObject.getJSONObject("result");
+		assertEquals(version, result.getInt("version"));
+		assertEquals(protocolVersion, result.getInt("protocolversion"));
+		assertEquals(walletVersion, result.getInt("walletversion"));
+		assertEquals(0.00000000, result.getDouble("balance"), 0);
+		assertEquals("", result.getString("errors"));
 	}
 
 	@Test
 	public void testGetPeerInfo() throws Exception {
 		Wallet w = new Wallet();
 		String actual = w.getPeerInfo();
-		assertTrue(actual.toString().contains("\"banscore\":0"));
-		assertTrue(actual.toString().contains("\"inbound\":true"));
-//		assertEquals(expected, new JSONObject(actual));
+		JSONObject actualJSONObject = new JSONObject(actual);
+		assertEquals(JSONObject.NULL, actualJSONObject.get("error"));
+        JSONArray result = actualJSONObject.getJSONArray("result");
+        for (int idx = 0; idx < result.length(); idx++) {
+            JSONObject curr = result.getJSONObject(idx);
+            assertEquals(protocolVersion, curr.getInt("version"));
+            assertEquals(0, curr.getInt("banscore"));
+            curr.getBoolean("inbound"); // should be able to get
+        }
 	}
 	
+	@Test
+	public void testGetBlockCount() throws Exception {
+		Wallet w = new Wallet();
+		String actual = w.getBlockCount();
+		JSONObject actualJSONObject = new JSONObject(actual);
+		assertEquals(JSONObject.NULL, actualJSONObject.get("id"));
+		assertEquals(JSONObject.NULL, actualJSONObject.get("error"));
+		assertTrue(actualJSONObject.getInt("result") > 0);
+    }
+
+	@Test
+	public void testGetConnectionCount() throws Exception {
+		Wallet w = new Wallet();
+		String actual = w.getConnectionCount();
+		JSONObject actualJSONObject = new JSONObject(actual);
+		assertEquals(JSONObject.NULL, actualJSONObject.get("error"));
+		assertEquals(JSONObject.NULL, actualJSONObject.get("id"));
+		assertTrue(actualJSONObject.getInt("result") > 0);
+    }
+
+	@Test
+	public void testGetDifficulty() throws Exception {
+		Wallet w = new Wallet();
+		String actual = w.getDifficulty();
+		JSONObject actualJSONObject = new JSONObject(actual);
+		assertEquals(JSONObject.NULL, actualJSONObject.get("error"));
+		assertEquals(JSONObject.NULL, actualJSONObject.get("id"));
+		assertTrue(actualJSONObject.getInt("result") > 0);
+    }
+
+	@Test
+	public void testGetGenerate() throws Exception {
+		Wallet w = new Wallet();
+		String actual = w.getDifficulty();
+		JSONObject actualJSONObject = new JSONObject(actual);
+		assertEquals(JSONObject.NULL, actualJSONObject.get("error"));
+		assertEquals(JSONObject.NULL, actualJSONObject.get("id"));
+		assertTrue(actualJSONObject.getInt("result") > 0);
+    }
+
+    @Test
+    public void testGetHashesPerSec() throws Exception {
+		Wallet w = new Wallet();
+		String actual = w.getHashesPerSec();
+		JSONObject actualJSONObject = new JSONObject(actual);
+		assertEquals(JSONObject.NULL, actualJSONObject.get("error"));
+		assertEquals(JSONObject.NULL, actualJSONObject.get("id"));
+		assertTrue(actualJSONObject.getInt("result") == 0);
+    }
+
+    @Test
+    public void testGetMiningInfo() throws Exception {
+		Wallet w = new Wallet();
+		String actual = w.getMiningInfo();
+		JSONObject actualJSONObject = new JSONObject(actual);
+        log.info("actualJSONObject:" + actualJSONObject);
+		JSONObject result = actualJSONObject.getJSONObject("result");
+        assertEquals(JSONObject.NULL, actualJSONObject.get("error"));
+        assertEquals(JSONObject.NULL, actualJSONObject.get("id"));
+		assertTrue(result.getInt("pooledtx") > 0);
+		assertEquals("", result.getString("errors"));
+		assertEquals(0, result.getInt("currentblocktx"));
+		assertEquals(false, result.getBoolean("generate"));
+		assertEquals(false, result.getBoolean("testnet"));
+		assertEquals(0, result.getInt("currentblocksize"));
+		assertEquals(0, result.getInt("hashespersec"));
+		assertTrue(result.getDouble("difficulty") > 0);
+		assertTrue(result.getDouble("blocks") > 0);
+		assertTrue(result.getLong("networkhashps") > 0);
+    }
 }
